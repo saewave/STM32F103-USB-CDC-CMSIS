b25f9f9ade0f84c94f0579247e4c94f9d24f6e5e
SaeWave <saewave@users.noreply.github.com>
2018-04-01 18:54:51 +0300

    Add license header

--- Src/main.c
+++ Src/main.c
@@ -1,3 +1,21 @@
+/* 
+ * This file is part of the SaeWave RemoteSwitch (USB-CDC-CMSIS) 
+ * distribution (https://github.com/saewave/STM32F103-USB-CDC-CMSIS).
+ * Copyright (c) 2017 Samoilov Alexey.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "stm32f10x.h"
 #include "usblib.h"
 
